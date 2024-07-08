f3ed4542d06ce335e79d54aa8b1d3f763ac01457
leeheejin0721 <dlgmlwls0000@naver.com>
2024-07-08 19:42:58 +0900

    0708 0742

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ axios.get('./data/menu.json')
         // 데이터를 담을 요소를 선택합니다.
         const fastContainer = document.querySelector('.fast');
         const priceContainer = document.querySelector('.price');
+        const donutContainer = document.querySelector('.donet');
 
         // 데이터를 반복하여 요소를 생성합니다.
         menus.forEach(menu => {
@@ -27,6 +28,18 @@ axios.get('./data/menu.json')
                 <p><b>${menu.price}</b>원</p>
             `;
             priceContainer.appendChild(priceDiv);
+
+            // 도넛 데이터 처리
+            if (menu.id >= 6) {
+                const donutDiv = document.createElement('div');
+                donutDiv.className = `donut${menu.id - 5}`;
+                donutDiv.innerHTML = `
+                    <img src="${menu.image}" alt="donut${menu.id - 5}">
+                    <p>${menu.name}</p>
+                    <p><b>${menu.price}</b>원</p>
+                `;
+                donutContainer.appendChild(donutDiv);
+            }
         });
     })
     .catch(err => {
